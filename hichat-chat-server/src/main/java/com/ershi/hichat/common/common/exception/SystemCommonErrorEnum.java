85dd8af3ddcd7dea116fcd41bf992263beaf1001
guershi <2402554312@qq.com>
2025-01-15 14:31:33 +0800

    fix: 修复参数校验异常不显示具体错误内容的bug

--- hichat-chat-server/src/main/java/com/ershi/hichat/common/common/exception/SystemCommonErrorEnum.java
+++ hichat-chat-server/src/main/java/com/ershi/hichat/common/common/exception/SystemCommonErrorEnum.java
@@ -11,7 +11,7 @@ import lombok.AllArgsConstructor;
 public enum SystemCommonErrorEnum implements ErrorEnum {
 
     SYSTEM_ERROR(-1, "系统出小差了，请稍后再试哦~"),
-    PARAM_VALID(-2, "参数校验失败"),
+    PARAM_VALID(-2, "参数校验失败，{0}"),
     LOCK_LIMIT(-3, "请求太频繁了，请稍后再试哦"),
     ;
 
